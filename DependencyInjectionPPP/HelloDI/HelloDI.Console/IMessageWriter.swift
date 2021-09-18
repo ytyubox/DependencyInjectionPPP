d009b2f99886950670929a6deb4db56a77277aef
Tsungyu Yu <ytyubox@gmail.com>
2021-09-18 20:28:17 +0800

    HelloDIConsoleIMessageWriter

--- DependencyInjectionPPP/HelloDI/HelloDI.Console/IMessageWriter.swift
+++ DependencyInjectionPPP/HelloDI/HelloDI.Console/IMessageWriter.swift
@@ -1,8 +1,3 @@
-namespace Ploeh.Samples.HelloDI.Console
-{
-    // ---- Code Section 1.2.1 ----
-    public interface IMessageWriter
-    {
-        void Write(string message);
-    }
+protocol HelloDIConsoleIMessageWriter {
+    func write(message: String)
 }
