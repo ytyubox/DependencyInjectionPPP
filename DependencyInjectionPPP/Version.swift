23df4c68962a5a569704b880f17ac7bb124708d5
Tsungyu Yu <ytyubox@gmail.com>
2021-09-18 19:44:00 +0800

    Initial Commit

---
+++ DependencyInjectionPPP/Version.swift
@@ -0,0 +1,11 @@
+//
+/* 
+ *		Created by 游宗諭 in 2021/9/18
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 12.0
+ */
+
+
+import Foundation
---
+++ DependencyInjectionPPPTests/DependencyInjectionPPPTests.swift
@@ -0,0 +1,19 @@
+//
+/* 
+ *		Created by 游宗諭 in 2021/9/18
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 12.0
+ */
+
+
+import XCTest
+@testable import DependencyInjectionPPP
+
+class DependencyInjectionPPPTests: XCTestCase {
+    func test() throws {
+        
+    }
+
+}
